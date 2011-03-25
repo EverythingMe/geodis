fc6f3c3c8ae927067b8a6ba2c3c79d547a1c19d8
dvir volk <dvir@doit9.com>
2011-03-26 01:59:47 +0200

    Added benchmarks

---
+++ test/benchmark.py
@@ -0,0 +1,46 @@
+#performance benchmarks
+
+import sys
+import os
+import redis
+from iprange import IPRange
+from location import Location
+import time
+
+def benchResolveIPs(num):
+
+    ips = ['166.205.138.92', '62.0.18.221',  '69.147.125.65', '188.127.241.156']
+    r = redis.Redis()
+    nips = len(ips)
+    for i in xrange(num):
+        ip = ips[i % nips]
+        loc = IPRange.getLocation(ip, r)
+        
+    return num
+
+def benchResolveCoords(num):
+
+    coords = [(-3.03333,53.47778), (40.7226,-74.66544), (31.78199,35.2196), (0,0),(45,45)]
+    r = redis.Redis()
+    ncoords = len(coords)
+    for i in xrange(num):
+        lat,lon = coords[i % ncoords]
+        loc = Location.getByLatLon(lat,lon, r)
+        
+
+    return num
+
+def benchSingleProc(func, num):
+
+    print "Running benchmark %s for %d times..." % (func.__name__, num)
+    st = time.time()
+    num = func(num)
+    et = time.time()
+
+    print "time: %.03fsec, rate: %.03fq/s" % (et - st, (float(num) / (et-st)))
+    
+if __name__ == "__main__":
+
+
+   benchSingleProc(benchResolveCoords, 10000)
+   benchSingleProc(benchResolveIPs, 10000)
