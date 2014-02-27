537fb50c2167eb2131b1b374f531a270d033377b
Dvir Volk <dvir@everything.me>
2014-02-27 15:35:21 +0200

    Moved src to geodis, and added requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from distutils.core import setup
 setup(name='geodis',
     version='1.0',
-    package_dir={'geodis': 'src'},
     packages=['geodis', 'geodis.provider'],
-    scripts=['src/geodis'],
+    scripts=['geodis/geodis'],
     requires=['redis(>=2.7.1)', 'geohasher', 'upoints'],
 #   url='https://github.com/EverythingMe/kickass-redis',
     author='Dvir Volk',
