23982c991e2e6a41d18447b88e5ee376462274c4
Omri Bahumi <omrib@everything.me>
2014-03-04 15:01:55 +0200

    Version freeze geohasher, upoints (both in requirements.txt and setup.py)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(name='geodis',
     version='1.0',
     packages=['geodis', 'geodis.provider'],
     scripts=['geodis/geodis'],
-    install_requires=['redis>=2.7.1', 'geohasher', 'upoints'],
+    install_requires=['redis>=2.7.1', 'geohasher==0.1dev', 'upoints==0.12.2'],
 #   url='https://github.com/EverythingMe/kickass-redis',
     author='Dvir Volk',
     description= 'Geodis - a redis based geo tagging and geo resolving library'
