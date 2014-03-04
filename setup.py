ef43a16c024fdc99cc2d6e927e2587eb7ab2a675
Omri Bahumi <omrib@everything.me>
2014-03-04 14:57:52 +0200

    fixed setup.py to install requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='geodis',
     version='1.0',
     packages=['geodis', 'geodis.provider'],
     scripts=['geodis/geodis'],
-    requires=['redis(>=2.7.1)', 'geohasher', 'upoints'],
+    install_requires=['redis>=2.7.1', 'geohasher', 'upoints'],
 #   url='https://github.com/EverythingMe/kickass-redis',
     author='Dvir Volk',
     description= 'Geodis - a redis based geo tagging and geo resolving library'
