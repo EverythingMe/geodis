155af6ed0a64022eec15068ada248454c196955a
Dvir Volk <dvir@everything.me>
2014-03-24 18:05:59 +0200

    added util founctions to country codes etc

--- test/testGeodis.py
+++ test/testGeodis.py
@@ -1,14 +1,15 @@
 import sys,os; sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../src')
 import unittest
 import redis
-from provider.geonames import GeonamesImporter
-from provider.ip2location import IP2LocationImporter
-from provider.zipcodes import ZIPImporter
+from geodis.provider.geonames import GeonamesImporter
+from geodis.provider.ip2location import IP2LocationImporter
+from geodis.provider.zipcodes import ZIPImporter
 
 
-from city import City
-from iprange import IPRange
-from zipcode import ZIPCode
+from geodis.city import City
+from geodis.iprange import IPRange
+from geodis.zipcode import ZIPCode
+from geodis import countries
 
 class  TestGeodis(unittest.TestCase):
     def setUp(self):
@@ -21,7 +22,7 @@ class  TestGeodis(unittest.TestCase):
         importer = GeonamesImporter('./data/locations.csv', self.redisHost, self.redisPort, self.redisDB)
         self.assertTrue(importer.runImport() > 0, 'Could not import cities csv')
 
-        
+
 
     def test2_ImportIP2Location(self):
 
@@ -30,43 +31,43 @@ class  TestGeodis(unittest.TestCase):
 
 
     def test3_ImportZIP(self):
-        
+
         importer = ZIPImporter('./data/zipcodes.csv', self.redisHost, self.redisPort, self.redisDB)
         self.assertTrue(importer.runImport() > 0, 'Could not import zipcodes csv')
 
     def test4_resolve(self):
         r = redis.Redis(self.redisHost, self.redisPort, self.redisDB)
         #resolve by coords
-        
+
         loc = City.getByLatLon(34.05223, -118.24368, r)
-        
+
         self.assertTrue(loc is not None)
-        
+
         self.assertTrue(loc.country == 'United States')
         self.assertTrue(loc.state == 'CA' or loc.state == 'California')
-        
+
         #resolve by textual search
-        
+
         locs = City.getByName('springfield', r, 44.0462, -123.022, 'united states')
-        
+
         self.assertTrue(len(locs)>0)
         self.assertTrue(locs[0].country == 'United States')
         self.assertTrue(locs[0].name == 'Springfield')
         self.assertTrue(locs[0].state == 'Oregon')
-        
-        
+
+
         #resolve by ip
         ip = '4.3.68.1'
 
         loc = IPRange.getCity(ip, r)
-        
+
         self.assertTrue(loc is not None)
         self.assertTrue(loc.country == 'United States')
         self.assertTrue(loc.state == 'CA' or loc.state == 'California')
 
         #resolve zip by lat,lon
         loc = ZIPCode.getByLatLon(34.0452, -118.284, r)
-        
+
         self.assertTrue(loc is not None)
         self.assertTrue(loc.name == '90006')
         self.assertTrue(loc.country == 'United States')
@@ -78,14 +79,26 @@ class  TestGeodis(unittest.TestCase):
         self.assertTrue(loc.name == '90001')
         self.assertTrue(loc.country == 'United States')
         self.assertTrue(loc.state == 'CA' or loc.state == 'California')
-        
-        
-        
-        
 
-        
+
+
+
+    def testCountryUtils(self):
+
+        testData = [('Israel', 'IL', 294640), ( "Sweden", 'SE', '2661886'), ('United States', 'US', 6252001)]
+
+        for name, code, cid in testData:
+
+            self.assertEqual(countries.getCountryCodeByName(name), code)
+            self.assertEqual(countries.getCountryNameByCode(code), name)
+            self.assertEqual(countries.getCountryIdByName(name), int(cid))
+            self.assertEqual(countries.getCountryIdByCode(code), int(cid))
+
+            self.assertEqual(countries.getCountryNameById(cid), name)
+            self.assertEqual(countries.getCountryCodeById(cid), code)
+
+
 if __name__ == '__main__':
     
-    import sys;sys.argv = ['', 'TestGeodis.test1_ImportGeonames']
+    import sys;sys.argv = ['', 'TestGeodis.testCountryUtils']
     unittest.main()
-
