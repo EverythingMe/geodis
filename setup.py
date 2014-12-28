d313910ec249109710cd07cfb42d2c06be5e38ff
Dvir Volk <dvir@everything.me>
2014-12-28 13:33:28 +0200

    Fixed setup.py so we can now install it from pip

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-import os
-
-install_requires = [str(ir.req) for ir in parse_requirements(os.path.abspath(
-                    os.path.join(__file__, '..', 'requirements.txt')))]
 
 setup(
     name='geodis',
-    version='1.0',
+    version='2.0.2',
     author='DoAT Media LTD.',
     author_email='dvirsky@gmail.com',
     url='https://github.com/doat/geodis',
+    scripts=['geodis/geodis'],
     packages=find_packages(),
-    install_requires=install_requires
+    install_requires=['redis>=2.7.1', 'geohasher==0.1dev', 'upoints==0.12.2']
+
 )
 
 
